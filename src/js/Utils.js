6ab8b219c3aaead43694daf53b7c517b86a8d196
Igor Rakoch <igorrakoch@gmail.com>
2024-04-14 22:19:30 -0400

    game ready, before neural network

--- src/js/Utils.js
+++ src/js/Utils.js
@@ -3,9 +3,9 @@ function lerp (a, b, t) {
 }
 
 function getIntersection (A,B,C,D) {
-    const tTop = (D.x - C.x) * (A.y - C.y) - (D.x );
-    const uTop = (C.y - A.y) * (A.x - B.x) - (C.x );
-    const bottom = (D.y - C.y) * (B.x - A.x) - (C.x);
+    const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
+    const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
+    const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
 
     if (bottom != 0) {
         const t = tTop / bottom;
@@ -17,6 +17,22 @@ function getIntersection (A,B,C,D) {
                 offset: t
             }
         }
-        return null;
     }
+    return null;
+}
+
+function polyIntesect (poly1, poly2) {
+    for (let i = 0; i < poly1.length; i++) {
+        for (let j = 0; j < poly2.length; j++) {
+            const touch = getIntersection(
+                poly1[i],
+                poly1[(i + 1) % poly1.length],
+                poly2[j],
+                poly2[(j + 1) % poly2.length]
+            );
+            if (touch)
+                return true;
+        }
+    }
+    return false;
 }
--- src/js/controls.js
+++ src/js/controls.js
@@ -1,11 +1,18 @@
 class Controls {
-    constructor() {
+    constructor(type) {
         this.forward=false;
         this.left  = false;
         this.right = false;
         this.reverse = false;
+        switch (type) {
+            case "KEYS" :
+                this.#addKeyboardListeneres ();
+                break;
+            case "DUMMY":
+                this.forward=true;
+                break;
+        }
 
-        this.#addKeyboardListeneres ();
     }
 
     #addKeyboardListeneres () {
