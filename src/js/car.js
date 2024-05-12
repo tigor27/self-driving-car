a08792536eb5baf3ab961d198c5e1f5154baa17b
Igor Rakoch <igorrakoch@gmail.com>
2024-05-12 18:57:32 -0400

    Network completed. Issue - only one diretion

--- src/js/car.js
+++ src/js/car.js
@@ -12,13 +12,17 @@ class Car {
         this.damaged = false;
         this.angle = 0;
 
-        if (controlType != "DUMMY")
+        this.useBrain = controlType=="AI";
+
+        if (controlType != "DUMMY") {
             this.sensor = new Sensor(this);
+            this.brain = new NeuralNetwork([this.sensor.rayCount, 6,4]);
+        }
         this.controls = new Controls(controlType);
         this.polygon = this.#createPolygon();
     }
 
-    draw (ctx, color) {
+    draw (ctx, color, drawSensor = false) {
         if (this.damaged) {
             ctx.fillStyle="gray";
         } else {
@@ -31,7 +35,7 @@ class Car {
             ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
         }
         ctx.fill();
-        if (this.sensor)
+        if (this.sensor && drawSensor)
             this.sensor.draw(ctx);
     }
 
@@ -42,8 +46,18 @@ class Car {
             this.damaged = this.#assessDamage(roadBorders, traffic);
         }
 
-        if (this.sensor)
+        if (this.sensor) {
             this.sensor.update(roadBorders, traffic);
+            const offsets=this.sensor.readings.map(s => s==null?0:1-s.offset);
+            const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+
+            if (this.useBrain) {
+                this.controls.forward=outputs[0];
+                this.controls.left=outputs[1];
+                this.controls.right=outputs[2];
+                this.controls.reverse=outputs[3];
+            }
+        }
     }
 
     #assessDamage(roadBorders, traffic) {
--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,59 @@
-const canvas=document.getElementById ("myCanvas");
-canvas.height=window.innerHeight;
-canvas.width=200;
+const carCanvas=document.getElementById ("carCanvas");
+carCanvas.height=window.innerHeight;
+carCanvas.width=200;
 
-const ctx = canvas.getContext ("2d");
-const road = new Road(canvas.width / 2, canvas.width*0.9);
-const car = new Car (road.getLaneCenter(1), 100, 30, 50, "KEYS", 3);
+const networkCanvas=document.getElementById ("networkCanvas");
+networkCanvas.height=window.innerHeight;
+networkCanvas.width=400;
+
+const carCtx = carCanvas.getContext ("2d");
+const networkCtx = networkCanvas.getContext ("2d");
+
+const road = new Road(carCanvas.width / 2, carCanvas.width*0.9);
+const N=100;
+const cars = generateCars (N);
+//const car = new Car (road.getLaneCenter(1), 100, 30, 50, "AI", 3);
 const traffic = [
     new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)
 ];
 animate();
 
-function animate() {
+function generateCars (N) {
+    const cars=[];
+    for (let i = 0; i <= N; i++) {
+        cars.push (new Car(road.getLaneCenter(1), 100, 20,50,"AI"))
+    }
+    return cars;
+}
+
+function animate(time) {
     for (let i = 0; i < traffic.length; i++) {
         traffic[i].update(road.borders, []);
     }
-    car.update(road.borders, traffic);
-
-    canvas.height=window.innerHeight;
-
-    ctx.save();
-    ctx.translate (0, -car.y+canvas.height*0.7);
-
-    road.draw(ctx);
-
-    for (let i = 0; i < traffic.length; i++) {
-        traffic[i].draw(ctx, "red");
+    for (let i = 0; i < cars.length; i++) {
+        cars[i].update(road.borders, traffic);
     }
-    car.draw(ctx, "blue");
+    //car.update(road.borders, traffic);
+    const bestCar = cars.find (c=>c.y==Math.min(...cars.map(c=>c.y)));
+    carCanvas.height=window.innerHeight;
 
-    ctx.restore();
+    carCtx.save();
+    carCtx.translate (0, -bestCar.y+carCanvas.height*0.7);
+
+    road.draw(carCtx);
+    carCtx.globalAlpha=0.2;
+    for (let i = 0; i < traffic.length; i++) {
+        traffic[i].draw(carCtx, "blue");
+    }
+    for (let i = 0; i < cars.length; i++) {
+        cars[i].draw(carCtx, "red");
+    }
+
+    carCtx.globalAlpha=1;
+    bestCar.draw(carCtx, "blue", true);
+
+    carCtx.restore();
+    networkCtx.lineDashOffset=-time/50;
+    Visualizer.drawNetwork(networkCtx, bestCar.brain);
     requestAnimationFrame(animate);
 }
